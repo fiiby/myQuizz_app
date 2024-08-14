b70621fd9c89224e90ab3d1bdae9aa85bac93bae
unknown <toohanour@gmail.com>
2024-08-14 23:59:09 +0300

    add small changes

---
+++ main.js
@@ -0,0 +1,11 @@
+function getQuestions() {
+    let myRequest = new XMLHttpRequest();
+    myRequest.onreadystatechange = function() {
+
+    }
+
+
+myRequest.open("GET", "html_questions.json", true);
+myRequest.dend();
+
+}
